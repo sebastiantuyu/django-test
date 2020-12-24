a974137654c8a0091b8ff035491fd3e1abc0408d
sebastiantuyu <alextpineiro@gmail.com>
2020-12-24 13:41:24 -0600

    Creacion del primer modelo para la DB y el primera migrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
+""" Escribir moddelos del post """
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=16)
+
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+
+    bio = models.TextField()
+
+    bdate = models.DateField(blank=True,null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    last = models.DateTimeField(auto_now=True)
