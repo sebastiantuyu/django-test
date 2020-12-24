39af0ca6ad18261da88ef5a39b81e97a48a5a7eb
sebastiantuyu <alextpineiro@gmail.com>
2020-12-24 14:27:56 -0600

    Version SIN ningun modelo de Usuario

--- posts/models.py
+++ posts/models.py
@@ -3,17 +3,3 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=16)
-
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-
-    bio = models.TextField()
-
-    bdate = models.DateField(blank=True,null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    last = models.DateTimeField(auto_now=True)
