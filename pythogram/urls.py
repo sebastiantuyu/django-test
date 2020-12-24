1b35c8013adb8541fc00628ae5f4cbcd42d5d203
sebastiantuyu <alextpineiro@gmail.com>
2020-12-24 11:11:59 -0600

    Limpie la rama main para solo hacer cambios importantes aqui

--- pythogram/urls.py
+++ pythogram/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello_world, hi, FormatoJson
+from .views import FormatoJson
 
 
 urlpatterns = [
-    path('hello-world/', hello_world),
-    path('format-j-1/',hi),
     path('format-j-2/',FormatoJson),
     path('admin/', admin.site.urls),
 ]
--- pythogram/views.py
+++ pythogram/views.py
@@ -5,22 +5,6 @@ from django.http import HttpResponse,JsonResponse
 from datetime import datetime
 import json
 
-def hello_world(rquest):
-    """ Vistas de prueba para Django """
-    now = datetime.now().strftime('%b %dth, %Y - %H:%H hrs')
-    return HttpResponse("Probando las vistas {now}".format(now=str(now)))
-
-def hi(request):
-    """ Ejectuar consola ---> import pdb; pdb.set_trace()"""
-    numbers = request.GET['numbers'].split(',')
-    # numbers = [int(i) for i in request.GET['numbers']]
-    numbers = [int(i) for i in numbers]
-    sorted_int = sorted(numbers)
-    dic = {}
-    for i in numbers:
-        dic[str(i)] = [i]
-    response = JsonResponse(dic, safe=False)
-    return HttpResponse("Esto son los numeros {0}".format(response.content))
 
 def FormatoJson(request):
     numbers = request.GET['numbers'].split(',')
